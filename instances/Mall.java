3200a8e8c472ddd90f4557cb8ed0afb7e8c8b868
nadimigorlayashwanth <nyashwanth.zworkz@gmail.com>
2024-07-24 07:52:57 +0530

    second

---
+++ instances/Mall.java
@@ -0,0 +1,90 @@
+class Mall {
+    public Mall() {
+        System.out.println("Creating Mall");
+    }
+}
+
+class Helicopter {
+    public Helicopter() {
+        System.out.println("Creating Helicopter");
+    }
+}
+
+class Market {
+    public Market() {
+        System.out.println("Creating Market");
+    }
+}
+
+class Train {
+    public Train() {
+        System.out.println("Creating Train");
+    }
+}
+
+class Ring {
+    public Ring() {
+        System.out.println("Creating Ring");
+    }
+}
+
+class Auto {
+    public Auto() {
+        System.out.println("Creating Auto");
+    }
+}
+
+class Auditorium {
+    public Auditorium() {
+        System.out.println("Creating Auditorium");
+    }
+}
+
+class SpaceShip {
+    public SpaceShip() {
+        System.out.println("Creating SpaceShip");
+    }
+}
+
+class Alien {
+    public Alien() {
+        System.out.println("Creating Alien");
+    }
+}
+
+class Bullet {
+    public Bullet() {
+        System.out.println("Creating Bullet");
+    }
+}
+
+class Galaxy {
+    public Galaxy() {
+        System.out.println("Creating Galaxy");
+    }
+}
+
+class Spoon {
+    public Spoon() {
+        System.out.println("Creating Spoon");
+    }
+}
+
+class BlackHole {
+    public BlackHole() {
+        System.out.println("Creating BlackHole");
+    }
+}
+
+class Cake {
+    public Cake() {
+        System.out.println("Creating Cake");
+    }
+}
+
+class Wrapper {
+    public Wrapper() {
+        System.out.println("Creating Wrapper");
+    }
+}
+
---
+++ instances/MallRunner.java
@@ -0,0 +1,95 @@
+public class MallRunner {
+    public static void main(String[] args) {
+        System.out.println("Running the main method");
+
+        Mall ref1 = new Mall();
+        Mall ref2 = new Mall();
+        Mall ref3 = new Mall();
+        Mall ref4 = new Mall();
+        Mall ref5 = new Mall();
+
+        Helicopter helicopter1 = new Helicopter();
+        Helicopter helicopter2 = new Helicopter();
+        Helicopter helicopter3 = new Helicopter();
+        Helicopter helicopter4 = new Helicopter();
+        Helicopter helicopter5 = new Helicopter();
+
+        Market market1 = new Market();
+        Market market2 = new Market();
+        Market market3 = new Market();
+        Market market4 = new Market();
+        Market market5 = new Market();
+
+        Train train1 = new Train();
+        Train train2 = new Train();
+        Train train3 = new Train();
+        Train train4 = new Train();
+        Train train5 = new Train();
+
+        Ring ring1 = new Ring();
+        Ring ring2 = new Ring();
+        Ring ring3 = new Ring();
+        Ring ring4 = new Ring();
+        Ring ring5 = new Ring();
+
+        Auto auto1 = new Auto();
+        Auto auto2 = new Auto();
+        Auto auto3 = new Auto();
+        Auto auto4 = new Auto();
+        Auto auto5 = new Auto();
+
+        Auditorium auditorium1 = new Auditorium();
+        Auditorium auditorium2 = new Auditorium();
+        Auditorium auditorium3 = new Auditorium();
+        Auditorium auditorium4 = new Auditorium();
+        Auditorium auditorium5 = new Auditorium();
+
+        SpaceShip spaceShip1 = new SpaceShip();
+        SpaceShip spaceShip2 = new SpaceShip();
+        SpaceShip spaceShip3 = new SpaceShip();
+        SpaceShip spaceShip4 = new SpaceShip();
+        SpaceShip spaceShip5 = new SpaceShip();
+
+        Alien alien1 = new Alien();
+        Alien alien2 = new Alien();
+        Alien alien3 = new Alien();
+        Alien alien4 = new Alien();
+        Alien alien5 = new Alien();
+
+        Bullet bullet1 = new Bullet();
+        Bullet bullet2 = new Bullet();
+        Bullet bullet3 = new Bullet();
+        Bullet bullet4 = new Bullet();
+        Bullet bullet5 = new Bullet();
+
+        Galaxy galaxy1 = new Galaxy();
+        Galaxy galaxy2 = new Galaxy();
+        Galaxy galaxy3 = new Galaxy();
+        Galaxy galaxy4 = new Galaxy();
+        Galaxy galaxy5 = new Galaxy();
+
+        Spoon spoon1 = new Spoon();
+        Spoon spoon2 = new Spoon();
+        Spoon spoon3 = new Spoon();
+        Spoon spoon4 = new Spoon();
+        Spoon spoon5 = new Spoon();
+
+        BlackHole blackHole1 = new BlackHole();
+        BlackHole blackHole2 = new BlackHole();
+        BlackHole blackHole3 = new BlackHole();
+        BlackHole blackHole4 = new BlackHole();
+        BlackHole blackHole5 = new BlackHole();
+
+        Cake cake1 = new Cake();
+        Cake cake2 = new Cake();
+        Cake cake3 = new Cake();
+        Cake cake4 = new Cake();
+        Cake cake5 = new Cake();
+
+        Wrapper wrapper1 = new Wrapper();
+        Wrapper wrapper2 = new Wrapper();
+        Wrapper wrapper3 = new Wrapper();
+        Wrapper wrapper4 = new Wrapper();
+        Wrapper wrapper5 = new Wrapper();
+    }
+}
